44abcd70056c678eb75d468a2c7aaf0461f44ed5
Tim Harder <radhermit@gmail.com>
2024-02-13 05:48:09 -0700

    cli: simplify man page generation by avoiding env var usage

--- crates/cli/src/mangen.rs
+++ crates/cli/src/mangen.rs
@@ -1,7 +1,7 @@
 #![allow(unused)]
 
 use std::path::PathBuf;
-use std::{env, io};
+use std::{env, fs, io};
 
 use bugbite::service::ServiceKind;
 use clap::CommandFactory;
@@ -12,7 +12,9 @@ mod service;
 mod utils;
 
 fn main() -> io::Result<()> {
-    let out_dir = PathBuf::from(env::var("OUT_DIR").expect("OUT_DIR is not set"));
+    let args: Vec<_> = env::args().collect();
+    let out_dir = PathBuf::from(args.get(1).map(|x| x.as_str()).unwrap_or("man"));
+    fs::create_dir_all(&out_dir).expect("failed creating output directory");
     for kind in ServiceKind::iter() {
         let cmd = match kind {
             ServiceKind::BugzillaRestV1 => service::bugzilla::Command::command(),
