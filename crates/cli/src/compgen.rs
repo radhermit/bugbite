f6973951f95d6743aeb03ede497d66d4f6312ae2
Tim Harder <radhermit@gmail.com>
2024-02-15 05:15:34 -0700

    cli: add initial shell completion file generation via clap

---
+++ crates/cli/src/compgen.rs
@@ -0,0 +1,23 @@
+#![allow(unused)]
+
+use std::path::PathBuf;
+use std::{env, fs, io};
+
+use clap::{CommandFactory, ValueEnum};
+use clap_complete::Shell;
+
+mod macros;
+mod options;
+mod service;
+mod utils;
+
+fn main() -> io::Result<()> {
+    let args: Vec<_> = env::args().collect();
+    let out_dir = PathBuf::from(args.get(1).map(|x| x.as_str()).unwrap_or("shell"));
+    fs::create_dir_all(&out_dir).expect("failed creating output directory");
+    let mut cmd = options::Command::command();
+    for &shell in Shell::value_variants() {
+        clap_complete::generate_to(shell, &mut cmd, "bite", &out_dir)?;
+    }
+    Ok(())
+}
