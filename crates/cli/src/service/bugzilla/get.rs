fe530ef5993fb65553f4f5e6892c5d5035038e13
Tim Harder <radhermit@gmail.com>
2024-02-16 04:04:13 -0700

    cli: bugzilla: make `get` accept the same optional, boolish types

--- crates/cli/src/service/bugzilla/get.rs
+++ crates/cli/src/service/bugzilla/get.rs
@@ -2,6 +2,7 @@ use std::io::{stdout, Write};
 use std::process::ExitCode;
 
 use bugbite::client::bugzilla::Client;
+use clap::builder::BoolishValueParser;
 use clap::Args;
 
 use crate::macros::async_block;
@@ -9,17 +10,41 @@ use crate::macros::async_block;
 #[derive(Debug, Args)]
 #[clap(next_help_heading = "Get options")]
 struct Options {
-    /// disable attachments
-    #[arg(short = 'A', long)]
-    without_attachments: bool,
+    /// enable/disable attachments
+    #[arg(
+        short = 'A',
+        long,
+        default_value = "true",
+        num_args = 0..=1,
+        default_missing_value = "true",
+        value_parser = BoolishValueParser::new(),
+        hide_possible_values = true,
+    )]
+    attachments: Option<bool>,
 
-    /// disable comments
-    #[arg(short = 'C', long)]
-    without_comments: bool,
+    /// enable/disable comments
+    #[arg(
+        short = 'C',
+        long,
+        default_value = "true",
+        num_args = 0..=1,
+        default_missing_value = "true",
+        value_parser = BoolishValueParser::new(),
+        hide_possible_values = true,
+    )]
+    comments: Option<bool>,
 
-    /// show bug history
-    #[arg(short = 'H', long)]
-    with_history: bool,
+    /// enable/disable history
+    #[arg(
+        short = 'H',
+        long,
+        default_value = "false",
+        num_args = 0..=1,
+        default_missing_value = "true",
+        value_parser = BoolishValueParser::new(),
+        hide_possible_values = true,
+    )]
+    history: Option<bool>,
 }
 
 #[derive(Debug, Args)]
@@ -35,10 +60,10 @@ pub(super) struct Command {
 
 impl Command {
     pub(super) fn run(self, client: Client) -> anyhow::Result<ExitCode> {
-        let attachments = !self.options.without_attachments;
-        let comments = !self.options.without_comments;
-        let history = self.options.with_history;
-        let bugs = async_block!(client.get(&self.ids, attachments, comments, history))?;
+        let attachments = self.options.attachments.unwrap_or_default();
+        let comments = self.options.comments.unwrap_or_default();
+        let history = self.options.history.unwrap_or_default();
+        let bugs = async_block!(client.get(&self.ids, attachments, comments, history,))?;
         let mut stdout = stdout().lock();
 
         for bug in bugs {
