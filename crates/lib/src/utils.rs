cbae5c223e0a81d5c89ce231ed4ac51b4851483e
Tim Harder <radhermit@gmail.com>
2024-02-17 09:27:54 -0700

    lib: minor current_dir() simplification

--- crates/lib/src/utils.rs
+++ crates/lib/src/utils.rs
@@ -1,13 +1,10 @@
-use std::env;
-
 use camino::Utf8PathBuf;
 
 use crate::Error;
 
-/// Get the current working directory as [`Utf8PathBuf`].
+/// Get the current working directory as an absolute [`Utf8PathBuf`].
 pub fn current_dir() -> crate::Result<Utf8PathBuf> {
-    let dir = env::current_dir()
-        .map_err(|e| Error::InvalidValue(format!("can't get current dir: {e}")))?;
-    Utf8PathBuf::try_from(dir)
-        .map_err(|e| Error::InvalidValue(format!("invalid unicode path: {e}")))
+    Utf8PathBuf::from(".")
+        .canonicalize_utf8()
+        .map_err(|e| Error::InvalidValue(format!("invalid current working directory: {e}")))
 }
