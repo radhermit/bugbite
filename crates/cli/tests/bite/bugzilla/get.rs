5cf88a47f23656a9ee6fee530f6d88f09a2ed5aa
Tim Harder <radhermit@gmail.com>
2024-02-19 00:44:21 -0700

    cli: use mock server setup from bugbite

--- crates/cli/tests/bite/bugzilla/get.rs
+++ crates/cli/tests/bite/bugzilla/get.rs
@@ -1,27 +1,20 @@
 use std::{env, fs};
 
+use bugbite::test::TestServer;
 use predicates::prelude::*;
-use wiremock::matchers::any;
-use wiremock::{Mock, MockServer, ResponseTemplate};
 
 use crate::command::cmd;
 use crate::macros::build_path;
 
 #[tokio::test]
 async fn single_bug() {
+    let server = TestServer::new().await;
+    server.respond(200, "bugzilla/get/single-bug.json").await;
     let path = build_path!(env!("CARGO_MANIFEST_DIR"), "testdata");
-    let json = fs::read_to_string(path.join("bugzilla/get/single-bug.json")).unwrap();
-
-    let mock_server = MockServer::start().await;
-    let template = ResponseTemplate::new(200).set_body_raw(json.as_bytes(), "application/json");
-    Mock::given(any())
-        .respond_with(template)
-        .mount(&mock_server)
-        .await;
-
     let expected = fs::read_to_string(path.join("bugzilla/get/single-bug")).unwrap();
-    env::set_var("BUGBITE_BASE", mock_server.uri());
+    env::set_var("BUGBITE_BASE", server.uri());
     env::set_var("BUGBITE_SERVICE", "bugzilla-rest-v1");
+
     for subcmd in ["g", "get"] {
         cmd("bite")
             .arg(subcmd)
