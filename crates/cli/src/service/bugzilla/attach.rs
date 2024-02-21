0e0a9b4fc070034f044158c29c535c9fe1eacd49
Tim Harder <radhermit@gmail.com>
2024-02-21 14:07:09 -0700

    cli: bugzilla attach: modify created attachment values

--- crates/cli/src/service/bugzilla/attach.rs
+++ crates/cli/src/service/bugzilla/attach.rs
@@ -13,13 +13,13 @@ use crate::macros::async_block;
 #[derive(Debug, Args)]
 #[clap(next_help_heading = "Attachments options")]
 struct Options {
-    /// comment to add with the attachment
-    #[arg(short, long)]
-    comment: Option<String>,
-
     /// short description of the attachment
     #[arg(short, long)]
     summary: Option<String>,
+
+    /// comment to add with the attachment
+    #[arg(short, long)]
+    comment: Option<String>,
 
     /// specify the MIME type
     #[arg(short, long, conflicts_with = "patch")]
@@ -28,6 +28,10 @@ struct Options {
     /// attachment is a patch
     #[arg(short, long, conflicts_with = "mime")]
     patch: Option<bool>,
+
+    /// attachment is private
+    #[arg(short = 'P', long)]
+    private: Option<bool>,
 }
 
 #[derive(Debug, Args)]
@@ -49,7 +53,23 @@ impl Command {
     pub(super) fn run(&self, client: &Client) -> Result<ExitCode, bugbite::Error> {
         let ids: Vec<_> = self.ids.iter().flatten().copied().collect();
         let mut stdout = stdout().lock();
-        let attachment = CreateAttachment::new(ids, &self.path)?;
+
+        let mut attachment = CreateAttachment::new(ids, &self.path)?;
+        if let Some(value) = self.options.summary.as_ref() {
+            attachment.summary = value.clone()
+        }
+        if let Some(value) = self.options.comment.as_ref() {
+            attachment.comment = value.clone()
+        }
+        if let Some(value) = self.options.mime.as_ref() {
+            attachment.content_type = value.clone()
+        }
+        if let Some(value) = self.options.patch {
+            attachment.is_patch = value;
+        }
+        if let Some(value) = self.options.private {
+            attachment.is_private = value;
+        }
 
         let ids = async_block!(client.attach(attachment))?;
         let ids = ids.iter().map(|x| x.to_string()).join(", ");
