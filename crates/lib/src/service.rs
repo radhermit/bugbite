e3d34482c9dabdad5330380a641b4e56dbf4726b
Tim Harder <radhermit@gmail.com>
2024-03-25 06:10:58 -0600

    lib: drop `Service` prefix for Config display string

--- crates/lib/src/service.rs
+++ crates/lib/src/service.rs
@@ -82,7 +82,7 @@ impl Config {
 
 impl fmt::Display for Config {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Service: {} -- {}", self.kind(), self.base())
+        write!(f, "{} -- {}", self.kind(), self.base())
     }
 }
 
