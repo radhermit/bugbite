05c4846dc8d0fab16e0eca005ff4d662ffce477b
Tim Harder <radhermit@gmail.com>
2024-03-26 11:56:02 -0600

    cli: more example command parsing simplifications

--- crates/cli/src/test.rs
+++ crates/cli/src/test.rs
@@ -14,9 +14,9 @@ pub(crate) fn subcmd_parse_examples(command: &[&str]) {
     let help = cmd.render_long_help().to_string();
     for line in help.lines() {
         if let Some(example) = line.trim().strip_prefix("> ") {
-            for cmd in example.split(" | ").filter_map(|x| x.strip_prefix("bite")) {
+            for cmd in example.split('|').filter_map(|x| x.strip_prefix("bite")) {
                 let full_cmd = format!("bite {service} {cmd}");
-                let args = shlex::split(full_cmd.trim()).unwrap();
+                let args = shlex::split(&full_cmd).unwrap();
                 let result = Command::try_parse_from(args);
                 reset_stdin();
                 assert!(
