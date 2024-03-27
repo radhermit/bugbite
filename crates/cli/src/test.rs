f8b0713b53e28161beb53535a3b40a86fb334950
Tim Harder <radhermit@gmail.com>
2024-03-27 02:05:34 -0600

    cli: fix examples test to work with piped commands

--- crates/cli/src/test.rs
+++ crates/cli/src/test.rs
@@ -14,14 +14,14 @@ pub(crate) fn subcmd_parse_examples(command: &[&str]) {
     let help = cmd.render_long_help().to_string();
     for line in help.lines() {
         if let Some(example) = line.trim().strip_prefix("> ") {
-            for cmd in example.split('|').filter_map(|x| x.strip_prefix("bite")) {
+            for cmd in example.split(" | ").filter_map(|x| x.strip_prefix("bite ")) {
                 let full_cmd = format!("bite {service} {cmd}");
                 let args = shlex::split(&full_cmd).unwrap();
                 let result = Command::try_parse_from(args);
                 reset_stdin();
                 assert!(
                     result.is_ok(),
-                    "failed parsing: bite {example}: {}",
+                    "failed parsing: bite {cmd}\n{}",
                     result.unwrap_err()
                 );
             }
