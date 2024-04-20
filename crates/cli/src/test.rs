194a248818ea4d67e326656f1a2f9a665264c072
Tim Harder <radhermit@gmail.com>
2024-04-20 17:50:18 -0600

    cli: simplify testing all literal blocks for example command parsing

--- crates/cli/src/test.rs
+++ crates/cli/src/test.rs
@@ -18,19 +18,17 @@ pub(crate) fn subcmd_parse_doc(subcmds: &[&str]) {
     let file_name = format!("bite-{}.adoc", subcmds.iter().join("-"));
     let file = build_path!(env!("CARGO_MANIFEST_DIR"), "doc", &file_name);
     let doc = fs::read_to_string(file).unwrap();
-    for line in doc.lines() {
-        if line.starts_with(" bite") {
-            for cmd in line.trim().split(" | ").filter(|x| x.starts_with("bite ")) {
-                let args = shlex::split(cmd).unwrap();
-                // TODO: fix parse_args() to return errors for tests instead of exiting
-                let result = Command::parse_args(args);
-                reset_stdin();
-                assert!(
-                    result.is_ok(),
-                    "failed parsing: {cmd}\n{}",
-                    result.unwrap_err()
-                );
-            }
+    for line in doc.lines().filter(|x| x.starts_with(' ')) {
+        for cmd in line.trim().split(" | ").filter(|x| x.starts_with("bite ")) {
+            let args = shlex::split(cmd).unwrap();
+            // TODO: fix parse_args() to return errors for tests instead of exiting
+            let result = Command::parse_args(args);
+            reset_stdin();
+            assert!(
+                result.is_ok(),
+                "failed parsing: {cmd}\n{}",
+                result.unwrap_err()
+            );
         }
     }
 }
