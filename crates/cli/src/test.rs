243ece1ca557c4ecbf2cd51321e4f2db8eecd138
Tim Harder <radhermit@gmail.com>
2024-04-20 04:59:45 -0600

    cli: doc: use literal blocks for `bite` command examples
    Since console highlighting is generally pointless.

--- crates/cli/src/test.rs
+++ crates/cli/src/test.rs
@@ -15,25 +15,21 @@ pub(crate) fn subcmd_parse_doc(subcmds: &[&str]) {
         }
     }
 
-    let example_marker = "[source,console]";
     let file_name = format!("bite-{}.adoc", subcmds.iter().join("-"));
     let file = build_path!(env!("CARGO_MANIFEST_DIR"), "doc", &file_name);
     let doc = fs::read_to_string(file).unwrap();
-    let mut lines = doc.lines();
-    while let Some(line) = lines.next() {
-        if line.trim().starts_with(example_marker) {
-            if let Some(line) = lines.next() {
-                for cmd in line.split(" | ").filter(|x| x.starts_with("bite ")) {
-                    let args = shlex::split(cmd).unwrap();
-                    // TODO: fix parse_args() to return errors for tests instead of exiting
-                    let result = Command::parse_args(args);
-                    reset_stdin();
-                    assert!(
-                        result.is_ok(),
-                        "failed parsing: {cmd}\n{}",
-                        result.unwrap_err()
-                    );
-                }
+    for line in doc.lines() {
+        if line.starts_with(" bite") {
+            for cmd in line.trim().split(" | ").filter(|x| x.starts_with("bite ")) {
+                let args = shlex::split(cmd).unwrap();
+                // TODO: fix parse_args() to return errors for tests instead of exiting
+                let result = Command::parse_args(args);
+                reset_stdin();
+                assert!(
+                    result.is_ok(),
+                    "failed parsing: {cmd}\n{}",
+                    result.unwrap_err()
+                );
             }
         }
     }
