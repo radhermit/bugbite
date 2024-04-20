8612c8a807103d4b9146904a10c28afefd120a51
Tim Harder <radhermit@gmail.com>
2024-04-19 22:14:01 -0600

    cli: bugzilla attach: add initial external doc

--- crates/cli/src/service/bugzilla/attach.rs
+++ crates/cli/src/service/bugzilla/attach.rs
@@ -10,12 +10,10 @@ use itertools::Itertools;
 use strum::VariantNames;
 use tracing::info;
 
-use crate::utils::wrapped_doc;
-
 #[derive(Debug, Args)]
-#[clap(next_help_heading = "Attachments options")]
+#[clap(next_help_heading = "Attachment options")]
 struct Options {
-    /// comment to add with the attachment
+    /// attachment comment
     #[arg(short, long)]
     comment: Option<String>,
 
@@ -29,22 +27,6 @@ struct Options {
         hide_possible_values = true,
         value_parser = PossibleValuesParser::new(Compression::VARIANTS)
             .map(|s| s.parse::<Compression>().unwrap()),
-        long_help = wrapped_doc!("
-            Compress attachments.
-
-            The value must be the compression variant to use or can be skipped
-            to use the default compression: xz.
-
-            Examples modifying bug 10:
-            - compress attachment using the default compression type
-            > bite at 10 path/to/file --compress
-
-            - compress attachment using zstd
-            > bite at 10 path/to/file --compress zstd
-
-            Possible values: {}",
-            Compression::VARIANTS.join(", ")
-        )
     )]
     compress: Option<Compression>,
 
@@ -55,19 +37,6 @@ struct Options {
         num_args = 0..=1,
         default_missing_value = "1.0",
         conflicts_with_all = ["mime", "patch"],
-        long_help = wrapped_doc!("
-            Auto-compress attachments larger than a given size.
-
-            The value must be the file size limit in MB above which attachments
-            will be compressed, defaulting to 1MB when not given.
-
-            Examples modifying bug 10:
-            - auto-compress attachment using the default compression type and size limit
-            > bite at 10 path/to/file --auto-compress
-
-            - auto-compress attachment using zstd with 5MB size limit
-            > bite at 10 path/to/file --auto-compress 5 --compress zstd
-        ")
     )]
     auto_compress: Option<f64>,
 
@@ -78,47 +47,11 @@ struct Options {
         num_args = 0..=1,
         default_missing_value = "1000",
         conflicts_with_all = ["mime", "patch"],
-        long_help = wrapped_doc!("
-            Auto-truncate text attachments to a given number of lines.
-
-            The value must be the number of lines to which the file will be
-            truncated starting from the end, defaulting to 1000 lines when not
-            given.
-
-            This option works in coordination with --auto-compress using the
-            file size limit to trigger when a text file is truncated. If the
-            option is not specified the default value will be used for it.
-
-            Examples modifying bug 10:
-            - auto-truncate to 1000 lines
-            > bite at 10 path/to/file.txt --auto-truncate
-
-            - auto-truncate to 5000 lines and compress attachment using zstd
-            > bite at 10 path/to/file --auto-truncate 5000 --compress zstd
-        ")
     )]
     auto_truncate: Option<usize>,
 
     /// support directory targets
-    #[arg(
-        short,
-        long,
-        conflicts_with = "mime",
-        long_help = wrapped_doc!("
-            Support directory targets for attachments.
-
-            Targeting a directory will attach a compressed tarball of the given
-            path. Without this option enabled, directory attachments will cause
-            errors.
-
-            Examples modifying bug 10:
-            - attach compressed tarball
-            > bite at 10 path/to/dir --dir
-
-            - attach tarball compressed with zstd
-            > bite at 10 path/to/dir --dir --compress zstd
-        ")
-    )]
+    #[arg(short, long, conflicts_with = "mime")]
     dir: bool,
 
     /// specify the MIME type
@@ -126,18 +59,6 @@ struct Options {
         short,
         long,
         conflicts_with_all = ["compress", "auto_compress", "auto_truncate", "dir", "patch"],
-        long_help = wrapped_doc!("
-            Specify the MIME type of the attachment.
-
-            This option is unnecessary for regular usage since the MIME type is
-            automatically detected using `file` with a fallback to internal
-            inference of common file types; however, `file` often misidentifies
-            plain text file types so text/plain may need to be forcibly set.
-
-            Example modifying bug 10:
-            - attach build.log with forced MIME type
-            > bite at 10 path/to/build.log --mime text/plain
-        ")
     )]
     mime: Option<String>,
 
@@ -154,15 +75,7 @@ struct Options {
     private: bool,
 
     /// short description of the attachment
-    #[arg(
-        short,
-        long,
-        long_help = wrapped_doc!("
-            A short description of the attachment.
-
-            By default the file name is used when this is not specified.
-        ")
-    )]
+    #[arg(short, long)]
     summary: Option<String>,
 }
 
@@ -177,19 +90,7 @@ struct Arguments {
         num_args = 1,
         required = true,
         value_delimiter = ',',
-        value_name = "ID[,...]",
-        long_help = wrapped_doc!("
-            IDs or aliases of bugs to create attachments for.
-
-            Taken from standard input when `-`.
-
-            Example:
-            - attach to all matching bugs
-            > bite s bugbite -f id | bite at - path/to/file
-
-            - attach to multiple bugs
-            > bite at 3,4,5 file
-        ")
+        value_name = "ID[,...]"
     )]
     ids: Vec<MaybeStdinVec<String>>,
 
@@ -198,15 +99,6 @@ struct Arguments {
         display_order = 1,
         required = true,
         value_hint = ValueHint::FilePath,
-        long_help = wrapped_doc!("
-            Paths to attachment files.
-
-            Multiple attachments can be created by specifying multiple paths.
-
-            Example:
-            - attach multiple files
-            > bite at 3 file1 file2 file3
-        ")
     )]
     files: Vec<Utf8PathBuf>,
 }
@@ -259,6 +151,6 @@ mod tests {
 
     #[test]
     fn examples() {
-        subcmd_parse_examples(&["bugzilla", "attach"]);
+        subcmd_parse_doc(&["bugzilla", "attach"]);
     }
 }
