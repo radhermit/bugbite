1f1a9af5a41232abe3aa63095ca5b6d0a3a95d94
Tim Harder <radhermit@gmail.com>
2024-04-22 07:39:05 -0600

    cli: drop linked connection command support
    The current command line option design isn't conducive to supporting
    this because it still allows options like `-c/--connection` to be
    defined when using linked commands.
    
    Making the service parsing and subcommand injection support much more
    complex just to support this in a consistent manner isn't worth it.

--- crates/cli/src/command.rs
+++ crates/cli/src/command.rs
@@ -1,8 +1,6 @@
 use std::collections::HashSet;
 use std::env;
-use std::ffi::OsStr;
 use std::io::stderr;
-use std::path::Path;
 use std::process::ExitCode;
 
 use bugbite::client::Client;
@@ -96,19 +94,9 @@ impl ServiceCommand {
         let base = cmd.options.service.base.as_deref();
         let service = cmd.options.service.service;
 
-        // determine running binary name for connection determination
-        let command = args
-            .first()
-            .as_ref()
-            .map(Path::new)
-            .and_then(Path::file_name)
-            .and_then(OsStr::to_str)
-            .filter(|x| *x != env!("CARGO_BIN_NAME"))
-            .map(String::from);
-
         // determine service type
-        let (selected, base) = match (connection, base, service, command.as_deref()) {
-            (Some(name), _, _, _) | (None, None, None, Some(name)) => {
+        let (selected, base) = match (connection, base, service) {
+            (Some(name), _, _) => {
                 let (kind, base) = config
                     .get(name)
                     .map_err(|e| Command::error(ErrorKind::InvalidValue, e))?;
@@ -118,7 +106,7 @@ impl ServiceCommand {
                 }
                 (kind, base)
             }
-            (None, Some(base), Some(service), _) => (service, base.to_string()),
+            (None, Some(base), Some(service)) => (service, base.to_string()),
             _ => {
                 if services.contains(arg) || arg.starts_with('-') {
                     Command::try_parse_from(&args)?;
