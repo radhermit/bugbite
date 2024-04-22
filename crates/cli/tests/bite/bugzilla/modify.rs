b15fa7b412628056ac5f7b78fa72271643ffe8bd
Tim Harder <radhermit@gmail.com>
2024-04-22 09:51:52 -0600

    cli: bugzilla modify: add auth required test

--- crates/cli/tests/bite/bugzilla/modify.rs
+++ crates/cli/tests/bite/bugzilla/modify.rs
@@ -2,6 +2,8 @@ use predicates::prelude::*;
 
 use crate::command::cmd;
 
+use super::*;
+
 #[test]
 fn aliases() {
     for subcmd in ["m", "modify"] {
@@ -15,4 +17,15 @@ fn aliases() {
                 .success();
         }
     }
+}
+
+#[tokio::test]
+async fn auth_required() {
+    let _server = start_server().await;
+
+    cmd("bite modify 1 -A test")
+        .assert()
+        .stdout("")
+        .stderr(predicate::str::diff("Error: authentication required").trim())
+        .failure();
 }
