0a70ce376aeb460e9ffa7c8e9a9d14683d21384e
Tim Harder <radhermit@gmail.com>
2024-05-03 17:27:45 -0600

    cli: bugzilla update: add service integration test for multiple bugs

--- crates/cli/tests/bugzilla/update.rs
+++ crates/cli/tests/bugzilla/update.rs
@@ -1,4 +1,5 @@
 use bugbite::traits::RequestSend;
+use itertools::Itertools;
 use tempfile::tempdir;
 
 use crate::command::cmd;
@@ -45,4 +46,46 @@ async fn from_template() {
         .unwrap();
 
     assert_eq!(bug.summary.unwrap(), "new-summary");
+}
+
+#[tokio::test]
+async fn multiple_bugs() {
+    let id1 = SERVICE
+        .create()
+        .unwrap()
+        .summary("summary")
+        .component("TestComponent")
+        .product("TestProduct")
+        .description("description")
+        .send(&SERVICE)
+        .await
+        .unwrap();
+
+    let id2 = SERVICE
+        .create()
+        .unwrap()
+        .summary("summary")
+        .component("TestComponent")
+        .product("TestProduct")
+        .description("description")
+        .send(&SERVICE)
+        .await
+        .unwrap();
+
+    cmd!("bite bugzilla update {id1} {id2} -S new-summary")
+        .assert()
+        .success();
+
+    let (bug1, bug2) = SERVICE
+        .get(&[id1, id2], false, false, false)
+        .unwrap()
+        .send(&SERVICE)
+        .await
+        .unwrap()
+        .into_iter()
+        .collect_tuple()
+        .unwrap();
+
+    assert_eq!(bug1.summary.unwrap(), "new-summary");
+    assert_eq!(bug2.summary.unwrap(), "new-summary");
 }
