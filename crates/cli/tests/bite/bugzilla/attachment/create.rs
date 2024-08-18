4344c138f23140a0647d340ca84c8f9bdef72bef
Tim Harder <radhermit@gmail.com>
2024-08-18 01:53:22 -0600

    cli: bugzilla: add mocked attachment creation tests

--- crates/cli/tests/bite/bugzilla/attachment/create.rs
+++ crates/cli/tests/bite/bugzilla/attachment/create.rs
@@ -33,3 +33,50 @@ async fn auth_required() {
         .stderr(predicate::str::diff("Error: authentication required").trim())
         .failure();
 }
+
+#[tokio::test]
+async fn single_bug() {
+    let server = start_server_with_auth().await;
+    server
+        .respond(200, TEST_DATA.join("attachment/create/single.json"))
+        .await;
+
+    let file = NamedTempFile::new().unwrap();
+    let path = file.path().to_str().unwrap();
+
+    cmd("bite bugzilla attachment create 1")
+        .arg(path)
+        .assert()
+        .stdout("")
+        .stderr("")
+        .success();
+}
+
+#[tokio::test]
+async fn multiple_bugs() {
+    let server = start_server_with_auth().await;
+    server
+        .respond(200, TEST_DATA.join("attachment/create/multiple.json"))
+        .await;
+
+    let file = NamedTempFile::new().unwrap();
+    let path = file.path().to_str().unwrap();
+
+    // invalid command -- ID args must be in a single comma-separated string
+    cmd("bite bugzilla attachment create 1 2")
+        .arg(path)
+        .assert()
+        .stdout("")
+        .stderr(predicate::str::contains(
+            "Error: invalid attachment source: 2",
+        ))
+        .failure()
+        .code(1);
+
+    cmd("bite bugzilla attachment create 1,2")
+        .arg(path)
+        .assert()
+        .stdout("")
+        .stderr("")
+        .success();
+}
