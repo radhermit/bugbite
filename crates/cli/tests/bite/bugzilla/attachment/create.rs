ccb0263abd0c7e6fc5e3b392d6aef2c62200ddb7
Tim Harder <radhermit@gmail.com>
2024-08-18 02:07:40 -0600

    cli: bugzilla attachment create: add initial dir target tests

--- crates/cli/tests/bite/bugzilla/attachment/create.rs
+++ crates/cli/tests/bite/bugzilla/attachment/create.rs
@@ -1,5 +1,7 @@
+use std::fs;
+
 use predicates::prelude::*;
-use tempfile::NamedTempFile;
+use tempfile::{tempdir, NamedTempFile};
 
 use crate::command::cmd;
 
@@ -74,6 +76,64 @@ async fn multiple_bugs() {
         .code(1);
 
     cmd("bite bugzilla attachment create 1,2")
+        .arg(path)
+        .assert()
+        .stdout("")
+        .stderr("")
+        .success();
+}
+
+#[tokio::test]
+async fn dir_target() {
+    let server = start_server_with_auth().await;
+    server
+        .respond(200, TEST_DATA.join("attachment/create/single.json"))
+        .await;
+
+    let dir = tempdir().unwrap();
+    let path = dir.path().join("src");
+    let path = path.to_str().unwrap();
+    fs::create_dir(path).unwrap();
+
+    // invalid MIME type
+    cmd("bite bugzilla attachment create 1")
+        .arg(path)
+        .args(["--mime", "text/plain"])
+        .assert()
+        .stdout("")
+        .stderr(
+            predicate::str::diff("Error: MIME type invalid for directory targets: text/plain")
+                .trim(),
+        )
+        .failure()
+        .code(1);
+
+    // invalid MIME type
+    cmd("bite bugzilla attachment create 1")
+        .arg(path)
+        .arg("--patch")
+        .assert()
+        .stdout("")
+        .stderr(predicate::str::diff("Error: patch type invalid for directory targets").trim())
+        .failure()
+        .code(1);
+
+    // empty directory target
+    let err = format!("Error: empty directory target: {path}");
+    cmd("bite bugzilla attachment create 1")
+        .arg(path)
+        .assert()
+        .stdout("")
+        .stderr(predicate::str::diff(err).trim())
+        .failure()
+        .code(1);
+
+    // create files
+    fs::write(dir.path().join("src/test1"), "test1").unwrap();
+    fs::write(dir.path().join("src/test2"), "test2").unwrap();
+
+    // valid
+    cmd("bite bugzilla attachment create 1")
         .arg(path)
         .assert()
         .stdout("")
