dff561f1dd7caba85ce6d5e524ccc39a2d205016
Tim Harder <radhermit@gmail.com>
2024-08-20 23:48:54 -0600

    lib: move Ordered trait to parent types module and rename ordered module

--- crates/lib/src/types.rs
+++ crates/lib/src/types.rs
@@ -1,2 +1,93 @@
-pub mod ordered;
-pub use ordered::{Ordered, OrderedSet};
+use std::fmt::Debug;
+use std::hash::Hash;
+
+mod ordered_set;
+pub use ordered_set::OrderedSet;
+
+pub trait Ordered: Debug + PartialEq + Eq + PartialOrd + Ord + Clone + Hash {}
+impl<T> Ordered for T where T: Debug + PartialEq + Eq + PartialOrd + Ord + Clone + Hash {}
+
+macro_rules! make_set_traits {
+    ($($x:ty),+) => {$(
+        impl<T: Ordered> BitAnd<&$x> for &$x {
+            type Output = $x;
+
+            fn bitand(self, other: &$x) -> Self::Output {
+                (&self.0 & &other.0).into()
+            }
+        }
+
+        impl<T: Ordered> BitAndAssign<&$x> for $x {
+            fn bitand_assign(&mut self, other: &$x) {
+                self.0 = &self.0 & &other.0;
+            }
+        }
+
+        impl<T: Ordered> BitAndAssign<$x> for $x {
+            fn bitand_assign(&mut self, other: $x) {
+                self.0 = &self.0 & &other.0;
+            }
+        }
+
+        impl<T: Ordered> BitOr<&$x> for &$x {
+            type Output = $x;
+
+            fn bitor(self, other: &$x) -> Self::Output {
+                (&self.0 | &other.0).into()
+            }
+        }
+
+        impl<T: Ordered> BitOrAssign<&$x> for $x {
+            fn bitor_assign(&mut self, other: &$x) {
+                self.0 = &self.0 | &other.0;
+            }
+        }
+
+        impl<T: Ordered> BitOrAssign<$x> for $x {
+            fn bitor_assign(&mut self, other: $x) {
+                self.0 = &self.0 | &other.0;
+            }
+        }
+
+        impl<T: Ordered> BitXor<&$x> for &$x {
+            type Output = $x;
+
+            fn bitxor(self, other: &$x) -> Self::Output {
+                (&self.0 ^ &other.0).into()
+            }
+        }
+
+        impl<T: Ordered> BitXorAssign<&$x> for $x {
+            fn bitxor_assign(&mut self, other: &$x) {
+                self.0 = &self.0 ^ &other.0;
+            }
+        }
+
+        impl<T: Ordered> BitXorAssign<$x> for $x {
+            fn bitxor_assign(&mut self, other: $x) {
+                self.0 = &self.0 ^ &other.0;
+            }
+        }
+
+        impl<T: Ordered> Sub<&$x> for &$x {
+            type Output = $x;
+
+            fn sub(self, other: &$x) -> Self::Output {
+                (&self.0 - &other.0).into()
+            }
+        }
+
+        impl<T: Ordered> SubAssign<&$x> for $x {
+            fn sub_assign(&mut self, other: &$x) {
+                self.0 = &self.0 - &other.0;
+            }
+        }
+
+        impl<T: Ordered> SubAssign<$x> for $x {
+            fn sub_assign(&mut self, other: $x) {
+                self.0 = &self.0 - &other.0;
+            }
+        }
+    )+};
+}
+use make_set_traits;
--- crates/lib/src/types/ordered_set.rs
+++ crates/lib/src/types/ordered_set.rs
@@ -6,7 +6,6 @@
 // https://github.com/bluss/indexmap/issues/153
 
 use std::cmp::Ordering;
-use std::fmt::Debug;
 use std::hash::{Hash, Hasher};
 use std::ops::{
     BitAnd, BitAndAssign, BitOr, BitOrAssign, BitXor, BitXorAssign, Deref, DerefMut, Sub, SubAssign,
@@ -15,8 +14,7 @@ use std::ops::{
 use indexmap::IndexSet;
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
 
-pub trait Ordered: Debug + PartialEq + Eq + PartialOrd + Ord + Clone + Hash {}
-impl<T> Ordered for T where T: Debug + PartialEq + Eq + PartialOrd + Ord + Clone + Hash {}
+use super::{make_set_traits, Ordered};
 
 /// Ordered set that implements Ord and Hash.
 #[derive(Debug, Clone)]
@@ -142,90 +140,6 @@ where
     }
 }
 
-macro_rules! make_set_traits {
-    ($($x:ty),+) => {$(
-        impl<T: Ordered> BitAnd<&$x> for &$x {
-            type Output = $x;
-
-            fn bitand(self, other: &$x) -> Self::Output {
-                (&self.0 & &other.0).into()
-            }
-        }
-
-        impl<T: Ordered> BitAndAssign<&$x> for $x {
-            fn bitand_assign(&mut self, other: &$x) {
-                self.0 = &self.0 & &other.0;
-            }
-        }
-
-        impl<T: Ordered> BitAndAssign<$x> for $x {
-            fn bitand_assign(&mut self, other: $x) {
-                self.0 = &self.0 & &other.0;
-            }
-        }
-
-        impl<T: Ordered> BitOr<&$x> for &$x {
-            type Output = $x;
-
-            fn bitor(self, other: &$x) -> Self::Output {
-                (&self.0 | &other.0).into()
-            }
-        }
-
-        impl<T: Ordered> BitOrAssign<&$x> for $x {
-            fn bitor_assign(&mut self, other: &$x) {
-                self.0 = &self.0 | &other.0;
-            }
-        }
-
-        impl<T: Ordered> BitOrAssign<$x> for $x {
-            fn bitor_assign(&mut self, other: $x) {
-                self.0 = &self.0 | &other.0;
-            }
-        }
-
-        impl<T: Ordered> BitXor<&$x> for &$x {
-            type Output = $x;
-
-            fn bitxor(self, other: &$x) -> Self::Output {
-                (&self.0 ^ &other.0).into()
-            }
-        }
-
-        impl<T: Ordered> BitXorAssign<&$x> for $x {
-            fn bitxor_assign(&mut self, other: &$x) {
-                self.0 = &self.0 ^ &other.0;
-            }
-        }
-
-        impl<T: Ordered> BitXorAssign<$x> for $x {
-            fn bitxor_assign(&mut self, other: $x) {
-                self.0 = &self.0 ^ &other.0;
-            }
-        }
-
-        impl<T: Ordered> Sub<&$x> for &$x {
-            type Output = $x;
-
-            fn sub(self, other: &$x) -> Self::Output {
-                (&self.0 - &other.0).into()
-            }
-        }
-
-        impl<T: Ordered> SubAssign<&$x> for $x {
-            fn sub_assign(&mut self, other: &$x) {
-                self.0 = &self.0 - &other.0;
-            }
-        }
-
-        impl<T: Ordered> SubAssign<$x> for $x {
-            fn sub_assign(&mut self, other: $x) {
-                self.0 = &self.0 - &other.0;
-            }
-        }
-    )+};
-}
-use make_set_traits;
 make_set_traits!(OrderedSet<T>);
 
 #[cfg(test)]
