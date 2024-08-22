b7e106c9bfec7ec7e9c8efba6b7a6b8c19f4b930
Tim Harder <radhermit@gmail.com>
2024-08-22 00:01:17 -0600

    lib: add initial tests for TimeDeltaOrStatic

--- crates/lib/src/time.rs
+++ crates/lib/src/time.rs
@@ -57,3 +57,30 @@ impl Api for TimeDeltaOrStatic {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use chrono::DateTime;
+
+    use crate::test::assert_err_re;
+
+    use super::*;
+
+    #[test]
+    fn parse() {
+        // invalid
+        for s in ["", "0", "1", "-1", "01:02:03"] {
+            let err = TimeDeltaOrStatic::from_str(s).unwrap_err();
+            assert_err_re!(err, format!("invalid time: {s}"));
+        }
+
+        // valid
+        for s in ["0000", "0001", "2020", "2020-08-09", "1y", "2m", "3d"] {
+            let time = TimeDeltaOrStatic::from_str(s).unwrap();
+            assert_eq!(time.to_string(), s);
+            assert_eq!(time.as_ref(), s);
+            let api = time.api();
+            assert!(DateTime::parse_from_rfc3339(&api).is_ok());
+        }
+    }
+}
