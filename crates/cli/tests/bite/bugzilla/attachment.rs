8724245ac28d9cd044cd37c2d9b4522920397d99
Tim Harder <radhermit@gmail.com>
2024-08-24 17:11:40 -0600

    cli: bugzilla attachment: add basic alias tests

--- crates/cli/tests/bite/bugzilla/attachment.rs
+++ crates/cli/tests/bite/bugzilla/attachment.rs
@@ -1,3 +1,22 @@
+use predicates::prelude::*;
+
+use crate::command::cmd;
+
 mod create;
 mod get;
 mod update;
+
+#[test]
+fn aliases() {
+    for subcmd in ["a", "attachment"] {
+        for opt in ["-h", "--help"] {
+            cmd("bite bugzilla")
+                .arg(subcmd)
+                .arg(opt)
+                .assert()
+                .stdout(predicate::str::is_empty().not())
+                .stderr("")
+                .success();
+        }
+    }
+}
