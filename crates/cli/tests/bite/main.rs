4f6504a9b930f2fc59e9ab33e9a4763c0eb704a4
Tim Harder <radhermit@gmail.com>
2024-08-24 19:07:08 -0600

    cli: add initial mocked tests for doc examples
    The unit tests verify parsing only while this runs the actual example
    command against default, valid mocked server responses. Obviously the
    output from the commands wouldn't match what is expected in most cases
    due to the static data used, but this should enable testing bugbite's
    entire request flow for valid responses through the encode, send, and
    decode phases to ensure the examples should work when pointed at a
    relevant service connection.

--- crates/cli/tests/bite/main.rs
+++ crates/cli/tests/bite/main.rs
@@ -1,9 +1,11 @@
-use std::env;
+use std::{env, fs};
 
 use bugbite::test::{build_path, TestServer};
 use camino::Utf8PathBuf;
+use itertools::Itertools;
 use once_cell::sync::Lazy;
 use predicates::prelude::*;
+use tempfile::tempdir;
 
 use command::cmd;
 
@@ -62,5 +64,67 @@ fn version() {
             .stdout(predicate::str::diff(format!("bite {version}")).trim())
             .stderr("")
             .success();
+    }
+}
+
+#[tokio::test]
+async fn doc() {
+    let server = start_server_with_auth().await;
+    let doc_dir = build_path!(env!("CARGO_MANIFEST_DIR"), "doc");
+    let data_dir = TEST_DATA_PATH.join("bugbite");
+
+    for entry in doc_dir.read_dir_utf8().unwrap() {
+        let entry = entry.unwrap();
+        let path = entry.path();
+        if path.extension().map(|x| x == "adoc").unwrap_or_default() {
+            let name = entry.file_name();
+            let stem = entry.path().file_stem().unwrap();
+            let cmd_args = stem.split('-').collect::<Vec<_>>();
+            let cmd_str = cmd_args.iter().join(" ");
+            let cmd_dir = data_dir.join(cmd_args.iter().skip(1).join("/"));
+            let data = cmd_dir.join("valid");
+
+            // skip commands without default, valid data response
+            if !data.exists() {
+                continue;
+            }
+
+            server.reset().await;
+            server.respond(200, data).await;
+
+            // use a temporary directory for current dir to drop any outputted files
+            let tmp_dir = tempdir().unwrap();
+            env::set_current_dir(&tmp_dir).unwrap();
+
+            let doc = fs::read_to_string(path).unwrap();
+            for (lineno, line) in doc.lines().enumerate().filter(|(_, x)| x.starts_with(' ')) {
+                for s in line.trim().split(" | ").filter(|x| x.starts_with(&cmd_str)) {
+                    let args = shlex::split(s).unwrap();
+
+                    // skip commands reading from stdin
+                    if args.iter().any(|x| x == "-") {
+                        continue;
+                    }
+
+                    // skip commands lacking service subcommands
+                    if args.len() < 3 {
+                        continue;
+                    }
+
+                    // skip commands with custom service options
+                    if args[2].starts_with('-') {
+                        continue;
+                    }
+
+                    let cmd_str = args.iter().join(" ");
+                    if let Err(e) = cmd(cmd_str).assert().try_success() {
+                        panic!(
+                            "failed running: {s}\nfile: {name}, line {}\n{e}",
+                            lineno + 1
+                        );
+                    }
+                }
+            }
+        }
     }
 }
