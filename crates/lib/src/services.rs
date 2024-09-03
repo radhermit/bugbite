878d9881b1653fc14ef96d515e2488beacb2ff60
Tim Harder <radhermit@gmail.com>
2024-09-03 08:39:40 -0600

    lib: drop outdated rustfmt skip directive

--- crates/lib/src/services.rs
+++ crates/lib/src/services.rs
@@ -7,7 +7,5 @@ use crate::service::Config;
 static SERVICES_DATA: &str = include_str!("../../../data/services.toml");
 
 /// Bundled service name to config mapping.
-#[rustfmt::skip]
-pub static SERVICES: Lazy<IndexMap<String, Config>> = Lazy::new(|| {
-    toml::from_str(SERVICES_DATA).unwrap()
-});
+pub static SERVICES: Lazy<IndexMap<String, Config>> =
+    Lazy::new(|| toml::from_str(SERVICES_DATA).unwrap());
