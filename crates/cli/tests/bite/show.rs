7b1280d9dd68b842f9dc288bcec716f04cd6db5d
Tim Harder <radhermit@gmail.com>
2024-09-05 06:17:18 -0600

    bite show: add custom config tests

--- crates/cli/tests/bite/show.rs
+++ crates/cli/tests/bite/show.rs
@@ -1,4 +1,7 @@
+use std::fs;
+
 use predicates::prelude::*;
+use tempfile::tempdir;
 
 use crate::command::cmd;
 
@@ -18,4 +21,66 @@ fn connections() {
         .stdout(predicate::str::contains("gentoo"))
         .stderr("")
         .success();
+}
+
+#[test]
+fn custom_config() {
+    let dir = tempdir().unwrap();
+    let home_path = dir.path().to_str().unwrap();
+    let dir = dir.path().join(".config");
+    let xdg_path = dir.to_str().unwrap();
+    let dir = dir.join("bugbite");
+    let dir_path = dir.to_str().unwrap();
+    fs::create_dir_all(dir_path).unwrap();
+    let file = dir.join("config");
+    let file_path = file.to_str().unwrap();
+    let config = indoc::indoc! {r#"
+        [bugzilla-test]
+        type = "bugzilla"
+        base = "http://127.0.0.1:8080/"
+    "#};
+    fs::write(file_path, config).unwrap();
+
+    // no custom config
+    cmd("bite show connections")
+        .assert()
+        .stdout(predicate::str::contains("bugzilla-test").not())
+        .stderr("")
+        .success();
+
+    // file target
+    cmd("bite show connections")
+        .env("BUGBITE_CONFIG", file_path)
+        .assert()
+        .stdout(predicate::str::contains("bugzilla-test"))
+        .stderr("")
+        .success();
+
+    // dir target
+    cmd("bite show connections")
+        .env("BUGBITE_CONFIG", dir_path)
+        .assert()
+        .stdout(predicate::str::contains("bugzilla-test"))
+        .stderr("")
+        .success();
+
+    if cfg!(target_os = "linux") {
+        // $HOME dir
+        cmd("bite show connections")
+            .env_remove("BUGBITE_CONFIG")
+            .env("HOME", home_path)
+            .assert()
+            .stdout(predicate::str::contains("bugzilla-test"))
+            .stderr("")
+            .success();
+
+        // xdg config dir
+        cmd("bite show connections")
+            .env_remove("BUGBITE_CONFIG")
+            .env("XDG_CONFIG_HOME", xdg_path)
+            .assert()
+            .stdout(predicate::str::contains("bugzilla-test"))
+            .stderr("")
+            .success();
+    }
 }
