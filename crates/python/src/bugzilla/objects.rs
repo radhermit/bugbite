242d3aecc64866877a28c0a02fb8105630c1065f
Tim Harder <radhermit@gmail.com>
2024-09-08 06:34:24 -0600

    python: add more getter field access for Bug wrapper objects

--- crates/python/src/bugzilla/objects.rs
+++ crates/python/src/bugzilla/objects.rs
@@ -10,6 +10,36 @@ impl Bug {
     fn id(&self) -> u64 {
         self.0.id
     }
+
+    #[getter]
+    fn assigned_to(&self) -> Option<&str> {
+        self.0.assigned_to.as_deref()
+    }
+
+    #[getter]
+    fn creator(&self) -> Option<&str> {
+        self.0.creator.as_deref()
+    }
+
+    #[getter]
+    fn summary(&self) -> Option<&str> {
+        self.0.summary.as_deref()
+    }
+
+    #[getter]
+    fn status(&self) -> Option<&str> {
+        self.0.status.as_deref()
+    }
+
+    #[getter]
+    fn resolution(&self) -> Option<&str> {
+        self.0.resolution.as_deref()
+    }
+
+    #[getter]
+    fn duplicate_of(&self) -> Option<u64> {
+        self.0.duplicate_of
+    }
 }
 
 impl From<bugzilla::Bug> for Bug {
