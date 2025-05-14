b51c3519defa4e13bef72a4032e4878c1bea4aff
Tim Harder <radhermit@gmail.com>
2025-05-14 16:21:06 -0600

    cargo: update dep versions

--- crates/python/src/utils.rs
+++ crates/python/src/utils.rs
@@ -2,12 +2,12 @@ use std::sync::OnceLock;
 
 use chrono::{DateTime, Utc};
 use pyo3::prelude::*;
-use pyo3::types::{PyDateTime, timezone_utc};
+use pyo3::types::{PyDateTime, PyTzInfo};
 
 /// Convert rust-based DateTime into PyDateTime.
 pub(crate) fn datetime(value: DateTime<Utc>, py: Python<'_>) -> PyResult<Bound<'_, PyDateTime>> {
     let value = value.timestamp() as f64;
-    let tz = timezone_utc(py);
+    let tz = PyTzInfo::utc(py)?;
     PyDateTime::from_timestamp(py, value, Some(&tz))
 }
 
