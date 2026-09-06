5cc5806bdc4d98ef9f5acb1f29f6a00307bf8c2e
Tim Harder <radhermit@gmail.com>
2026-09-05 19:33:29 -0600

    rework(bugzilla): simplify ids parameter for service requests

--- crates/lib/src/service/bugzilla/requests.rs
+++ crates/lib/src/service/bugzilla/requests.rs
@@ -1,5 +1,3 @@
-use std::fmt::Display;
-
 pub mod attachment;
 pub mod comment;
 pub mod create;
@@ -14,10 +12,10 @@ pub mod version;
 // public request creation API
 impl super::Bugzilla {
     /// Create a request to add attachments to the specified bugs.
-    pub fn attachment_create<I, S>(&self, ids: I) -> attachment::create::Request
+    pub fn attachment_create<I>(&self, ids: I) -> attachment::create::Request
     where
-        I: IntoIterator<Item = S>,
-        S: Display,
+        I: IntoIterator,
+        I::Item: std::fmt::Display,
     {
         attachment::create::Request::new(self.clone(), ids)
     }
@@ -31,10 +29,10 @@ impl super::Bugzilla {
     }
 
     /// Create a request to get attachments from the specified bugs.
-    pub fn attachment_get_item<I, S>(&self, ids: I) -> attachment::get_item::Request
+    pub fn attachment_get_item<I>(&self, ids: I) -> attachment::get_item::Request
     where
-        I: IntoIterator<Item = S>,
-        S: Display,
+        I: IntoIterator,
+        I::Item: std::fmt::Display,
     {
         attachment::get_item::Request::new(self.clone(), ids)
     }
@@ -53,19 +51,19 @@ impl super::Bugzilla {
     }
 
     /// Create a request to get the comments from the specified bugs.
-    pub fn comment_get<I, S>(&self, ids: I) -> comment::get::Request
+    pub fn comment_get<I>(&self, ids: I) -> comment::get::Request
     where
-        I: IntoIterator<Item = S>,
-        S: Display,
+        I: IntoIterator,
+        I::Item: std::fmt::Display,
     {
         comment::get::Request::new(self.clone(), ids)
     }
 
     /// Create a request to tag the comments from the specified bugs.
-    pub fn comment_tag<I, S>(&self, ids: I) -> comment::tag::Request
+    pub fn comment_tag<I>(&self, ids: I) -> comment::tag::Request
     where
-        I: IntoIterator<Item = S>,
-        S: Display,
+        I: IntoIterator,
+        I::Item: std::fmt::Display,
     {
         comment::tag::Request::new(self.clone(), ids)
     }
@@ -81,19 +79,19 @@ impl super::Bugzilla {
     }
 
     /// Create a request to get the specified bugs.
-    pub fn get<I, S>(&self, ids: I) -> get::Request
+    pub fn get<I>(&self, ids: I) -> get::Request
     where
-        I: IntoIterator<Item = S>,
-        S: Display,
+        I: IntoIterator,
+        I::Item: std::fmt::Display,
     {
         get::Request::new(self.clone(), ids)
     }
 
     /// Create a request to get the history of the specified bugs.
-    pub fn history<I, S>(&self, ids: I) -> history::Request
+    pub fn history<I>(&self, ids: I) -> history::Request
     where
-        I: IntoIterator<Item = S>,
-        S: Display,
+        I: IntoIterator,
+        I::Item: std::fmt::Display,
     {
         history::Request::new(self.clone(), ids)
     }
@@ -104,10 +102,10 @@ impl super::Bugzilla {
     }
 
     /// Create a request to update the specified bugs.
-    pub fn update<I, S>(&self, ids: I) -> update::Request
+    pub fn update<I>(&self, ids: I) -> update::Request
     where
-        I: IntoIterator<Item = S>,
-        S: Display,
+        I: IntoIterator,
+        I::Item: std::fmt::Display,
     {
         update::Request::new(self.clone(), ids)
     }
@@ -118,28 +116,28 @@ impl super::Bugzilla {
     }
 
     /// Create a request to create the specified users.
-    pub fn user_create<I, S>(&self, emails: I) -> user::create::Request
+    pub fn user_create<I>(&self, emails: I) -> user::create::Request
     where
-        I: IntoIterator<Item = S>,
-        S: Display,
+        I: IntoIterator,
+        I::Item: std::fmt::Display,
     {
         user::create::Request::new(self.clone(), emails)
     }
 
     /// Create a request to get the specified users.
-    pub fn user_get<I, S>(&self, ids: I) -> user::get::Request
+    pub fn user_get<I>(&self, ids: I) -> user::get::Request
     where
-        I: IntoIterator<Item = S>,
-        S: Display,
+        I: IntoIterator,
+        I::Item: std::fmt::Display,
     {
         user::get::Request::new(self.clone(), ids)
     }
 
     /// Create a request to update the specified users.
-    pub fn user_update<I, S>(&self, ids: I) -> user::update::Request
+    pub fn user_update<I>(&self, ids: I) -> user::update::Request
     where
-        I: IntoIterator<Item = S>,
-        S: Display,
+        I: IntoIterator,
+        I::Item: std::fmt::Display,
     {
         user::update::Request::new(self.clone(), ids)
     }
