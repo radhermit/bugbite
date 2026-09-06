2f6e77e9c2a2c5f3ab9bacf818e5e842f7b69b4f
Tim Harder <radhermit@gmail.com>
2026-09-05 19:30:42 -0600

    rework(lib): move request creation methods into service request modules

--- crates/lib/src/service/redmine.rs
+++ crates/lib/src/service/redmine.rs
@@ -178,17 +178,6 @@ impl Redmine {
         let base = self.config().web_base().as_str().trim_end_matches('/');
         format!("{base}/issues/{id}")
     }
-
-    pub fn get<I>(&self, ids: I) -> get::Request
-    where
-        I: IntoIterator<Item = u64>,
-    {
-        get::Request::new(self.clone(), ids)
-    }
-
-    pub fn search(&self) -> search::Request {
-        search::Request::new(self.clone())
-    }
 }
 
 /// Redmine REST API error response.
--- crates/lib/src/service/redmine/requests.rs
+++ crates/lib/src/service/redmine/requests.rs
@@ -1,2 +1,16 @@
 pub mod get;
 pub mod search;
+
+// public request creation API
+impl super::Redmine {
+    pub fn get<I>(&self, ids: I) -> get::Request
+    where
+        I: IntoIterator<Item = u64>,
+    {
+        get::Request::new(self.clone(), ids)
+    }
+
+    pub fn search(&self) -> search::Request {
+        search::Request::new(self.clone())
+    }
+}
